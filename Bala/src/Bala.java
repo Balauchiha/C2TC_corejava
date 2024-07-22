ccf09c301538854311f7085f10c8d7a663851503
Balauchiha <bala2004lpm@gmail.com>
2024-07-22 15:27:13 +0530

    good

---
+++ Bala/src/Bala.java
@@ -0,0 +1,12 @@
+
+public class Bala {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Bala/src/Muruga/Bala.java
@@ -0,0 +1,13 @@
+package Muruga;
+
+public class Bala {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
